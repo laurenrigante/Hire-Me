926c0e929f19afab2ea86fa689d88bdf6bf21cb5
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2024-04-09 11:44:13 -0400

    business images

--- hire-me/src/pages/FindFreelancer.jsx
+++ hire-me/src/pages/FindFreelancer.jsx
@@ -8,12 +8,27 @@ import SearchBar from "../components/SearchBar";
 import BusinessComponent from "../components/BusinessComponent";
 import Pagination from "../components/Pagination";
 
+
+import paintingImage from "../assets/business_images/painting_company.jpeg";
+import babysittingImage from "../assets/business_images/babysitting.jpeg";
+import renovationImage from "../assets/business_images/renovation.jpeg";
+import plumbingImage from "../assets/business_images/plumbing.jpg";
+import electricianImage from "../assets/business_images/electrician.jpg";
+import childcare from "../assets/business_images/cc.jpg";
+import contractor from "../assets/business_images/contractor.jpg";
+import plumbert from "../assets/business_images/plumbert.jpg";
+import bdayclown from "../assets/business_images/bdayclown.jpg";
+import dj from "../assets/business_images/dj.jpg";
+import wb from "../assets/business_images/web.jpg";
+import dogwalk from "../assets/business_images/dogwalk.jpg";
+import ps from "../assets/business_images/ps.png";
 const FindFreelancer = () => {
 
     
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(5); // Change this value according to your needs
+  //const [itemsPerPage] = useState(4); // Change this value according to your needs
 
+  const itemsPerPage=4;
     // Calculate index of the last item of the current page
     const indexOfLastItem = currentPage * itemsPerPage;
     // Calculate index of the first item of the current page
@@ -25,7 +40,7 @@ const FindFreelancer = () => {
     {
       businessName: "Markell Painting Services",
       freelanceType: "Painter",
-      picture: "https://example.com/profile.jpg",
+      picture: paintingImage,
       phoneNumber: "514-569-8475",
       email: "MarkellPainting@hotmail.com",
       businessId: "1",
@@ -34,7 +49,7 @@ const FindFreelancer = () => {
     {
       businessName: "Sarah's Babysitting",
       freelanceType: "Babysitter",
-      picture: "https://example.com/babysitting.jpg",
+      picture: babysittingImage,
       phoneNumber: "514-654-3210",
       email: "sarahReese@hotmail.com",
       businessId: "2",
@@ -42,7 +57,7 @@ const FindFreelancer = () => {
     {
       businessName: "Mark's Renovation Solutions",
       freelanceType: "Renovator , Contractor, HandyMan",
-      picture: "https://example.com/renovation.jpg",
+      picture:renovationImage,
       phoneNumber: "514-789-0123",
       email: "MRS@hotmail.com",
       businessId: "3",
@@ -50,7 +65,7 @@ const FindFreelancer = () => {
     {
       businessName: "Emily's Plumbing Services",
       freelanceType: "Plumber",
-      picture: "https://example.com/plumbing.jpg",
+      picture: plumbingImage,
       phoneNumber: "438-654-9870",
       email: "emilytheplumber@outlook.com",
       businessId: "4",
@@ -58,7 +73,7 @@ const FindFreelancer = () => {
     {
       businessName: "Jack Electric",
       freelanceType: "Electrician",
-      picture: "https://example.com/electrical.jpg",
+      picture: electricianImage,
       phoneNumber: "514-012-3456",
       email: "JEsolutions@live.com",
       businessId: "5",
@@ -66,7 +81,7 @@ const FindFreelancer = () => {
     {
       businessName: "Sophie's Childcare Services",
       freelanceType: "Babysitter, Daycare",
-      picture: "https://example.com/babysitting2.jpg",
+      picture: childcare,
       phoneNumber: "438-555-5555",
       email: "sophieDoone@example.com",
       businessId: "6",
@@ -74,7 +89,7 @@ const FindFreelancer = () => {
     {
       businessName: "Adam's Construction",
       freelanceType: "Contractor",
-      picture: "https://example.com/construction.jpg",
+      picture: contractor,
       phoneNumber: "514-222-3333",
       email: "adamPierce@acconstruction.com",
       businessId: "7",
@@ -82,7 +97,7 @@ const FindFreelancer = () => {
     {
       businessName: "David's Plumbing Services",
       freelanceType: "Plumber",
-      picture: "https://example.com/plumbing2.jpg",
+      picture: plumbert,
       phoneNumber: "438-888-9999",
       email: "davidplumbing@outlook.com",
       businessId: "8",
@@ -90,7 +105,7 @@ const FindFreelancer = () => {
     {
       businessName: "Happy Clown Entertainment",
       freelanceType: "Birthday Clown , Entertainment, Party",
-      picture: "https://example.com/clown.jpg",
+      picture: bdayclown,
       phoneNumber: "514-123-4567",
       email: "happyclown@example.com",
       businessId: "9",
@@ -98,7 +113,7 @@ const FindFreelancer = () => {
     {
       businessName: "Beats By DJ Mike",
       freelanceType: "DJ, Music, Party, Wedding",
-      picture: "https://example.com/dj.jpg",
+      picture: dj,
       phoneNumber: "438-987-6543",
       email: "djmike@example.com",
       businessId: "10",
@@ -106,7 +121,7 @@ const FindFreelancer = () => {
     {
       businessName: "The Wedding Band Company",
       freelanceType: "Wedding Band, Music, Entertainment",
-      picture: "https://example.com/weddingband.jpg",
+      picture: wb,
       phoneNumber: "555-222-3333",
       email: "weddingband@example.com",
       businessId: "11",
@@ -114,7 +129,7 @@ const FindFreelancer = () => {
     {
       businessName: "Paws & Play Dog Walking Services",
       freelanceType: "Dog Walker, Dog Walking",
-      picture: "https://example.com/dogwalker.jpg",
+      picture: dogwalk,
       phoneNumber: "438-888-9999",
       email: "pawsandplay@example.com",
       businessId: "12",
@@ -122,7 +137,7 @@ const FindFreelancer = () => {
     {
       businessName: "Pet Sitting Services",
       freelanceType: "Pets, Pet Sitting, Animals",
-      picture: "https://example.com/dogwalker.jpg",
+      picture: ps,
       phoneNumber: "438-888-9999",
       email: "Jessie_animal_lover@hotmail.com",
       businessId: "13",
@@ -142,7 +157,7 @@ const FindFreelancer = () => {
         </div>
         <div>
           {/* Map over the businessDataList and render BusinessComponent for each business */}
-          {businessData.map((businessData, index) => (
+          {currentBusinessData.map((businessData, index) => (
             <BusinessComponent key={index} businessData={businessData} />
           ))}
         </div>
